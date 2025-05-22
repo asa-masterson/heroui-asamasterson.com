1ec2418fb0a549a0e186bdc1c63b6a08169e4c65
Asa Masterson <asa.masterson@gmail.com>
2025-05-22 19:38:58 +0100

    ttl

--- heroui-asa/src/pages/index.tsx
+++ heroui-asa/src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function IndexPage() {
   const [viewCount, setViewCount] = useState<string | null>("Loading...");
 
   const fetchViewCount = () => {
-    fetch("https://a.bigfluffy.monster/counter/id/demo01")
+    fetch("https://a.bigfluffy.monster/counter/id/demo01?ttl=3600")
       .then((response) => response.json())
       .then((data) => {
         setViewCount("Views - " + data.Users);
