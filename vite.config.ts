40c90b373b34030d0274e6ef5645f98a19a7de93
Asa Masterson <asa.masterson@gmail.com>
2025-05-22 22:00:53 +0100

    Add Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { codecovVitePlugin } from "@codecov/vite-plugin";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
