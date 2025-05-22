509bc4a2732f662f35d138cc7d5e4b6997bc3e7d
Asa Masterson <asa.masterson@gmail.com>
2025-05-22 21:45:51 +0100

    reorder

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+      react(),
+    tsconfigPaths(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "heroui-asamasterson.com",
+      uploadToken: process.env.CODECOV_TOKEN,
+    })
+  ],
 })
