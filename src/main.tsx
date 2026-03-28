32d4d9e0716564eb3fc136df28225acb1a965299
Asa <Asa.Masterson@gmail.com>
2026-03-28 05:52:51 +0000

    new swetrix version 5.11

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import * as Sentry from "@sentry/browser";
+import * as Swetrix from "swetrix";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
@@ -11,6 +12,11 @@ import "@/styles/globals.css";
 Sentry.init({ 
   dsn: import.meta.env.VITE_GLITCHTIP_DSN
 });
+
+Swetrix.init("sjFU3ryURYdB", {
+  apiURL: "https://swetrix.bigfluffy.monster/backend/v1/log",
+});
+Swetrix.trackViews();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
