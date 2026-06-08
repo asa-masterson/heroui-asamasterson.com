278c4b6a25f4fb41ac74dfb3c6cd1f31722498ae
Asa <Asa.Masterson@gmail.com>
2026-06-08 23:52:25 +0100

    update sitemap to be dynamic using vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { codecovVitePlugin } from "@codecov/vite-plugin";
+import sitemap from 'vite-plugin-sitemap';
+
+const BASE_URL = 'https://asamasterson.com';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +15,14 @@ export default defineConfig({
       enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
       bundleName: "heroui-asamasterson.com",
       uploadToken: process.env.CODECOV_TOKEN,
+    }),
+    sitemap({
+      hostname: BASE_URL,
+      dynamicRoutes: ['/about', '/projects', '/pacman', '/pong', '/2048'],
+      changefreq: 'monthly',
+      priority: { '/': 1.0, '/about': 0.8, '/projects': 0.8, '/pacman': 0.6, '/pong': 0.6, '/2048': 0.6 },
+      outDir: 'dist',
+      generateRobotsTxt: false,
     }),
   ],
   ssgOptions: {
